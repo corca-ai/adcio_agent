1b7d11c11dc9e1b553728e490c0bf34d127706d3
adbr <adbr.dev@gmail.com>
2023-08-11 16:13:39 +0900

    feat: remove flavor example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -36,7 +36,6 @@ class HomePage extends StatelessWidget {
       ),
       body: AdcioAgent(
         clientId: '30cb6fd0-17a5-4c56-b144-fef67de81bef',
-        flavor: Flavor.dev,
         onClickProduct: (String productId) {
           log('productId = $productId');
 
